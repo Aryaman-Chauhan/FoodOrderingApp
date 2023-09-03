ca9ccb57003f208250a880993796cd928cf40b12
Arination <87356283+Arination@users.noreply.github.com>
2023-09-03 17:40:25 +0530

    sorted js frontend rendering

--- src/main/resources/static/JS_files/index.js
+++ src/main/resources/static/JS_files/index.js
@@ -1,34 +1,4 @@
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAMwwFDssC3yNpLSuhpbcxo_DonwHx3mag",
-//   authDomain: "oop-project-bf8eb.firebaseapp.com",
-//   projectId: "oop-project-bf8eb",
-//   storageBucket: "oop-project-bf8eb.appspot.com",
-//   messagingSenderId: "251672683615",
-//   appId: "1:251672683615:web:257f79229ed64f2fffe7ea",
-//   measurementId: "G-8RLXBGPKKZ"
-// };
-
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
-// const express = require("express");
-// const bodyparser = require("body-parser");
-// const menuBtn = document.querySelector(".menu-btn")
-// const navigation = document.querySelector(".navigation")
-
-// menuBtn.addEventListener("click", ()=>{
-//     menuBtn.classList.toggle("active");
-//     navigation.classList.toggle("active");
-// });
 //--------------------DATA RENDERING--------------------
 
 
